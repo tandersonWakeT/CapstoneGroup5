edda5f1d0e9c9c5326c66766602b75a0643b10ba
tim brady <timbrady@uri.edu>
2022-04-01 11:12:31 -0400

    test push

--- spring-boot-ecommerce/spring-boot-ecommerce2/src/main/java/com/hcl/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.ecommerce.service;
-
-import com.hcl.ecommerce.dto.Purchase;
-import com.hcl.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-	
-	PurchaseResponse placeOrder(Purchase purchase);
-}
